d97ac5a959ac85873efe1b98e7d39f7c3414c592
guilhermegalvan <46463386+guilhermegalvan@users.noreply.github.com>
2019-12-13 16:22:49 -0300

    Added mechanics
    Added player movements, animation, first weapon and projectile animations and behaviours.

---
+++ Abrubs Shooter/Assets/Scripts/Weapon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    private void Update()
+    {
+        
+        transform.rotation = Quaternion.identity;
+        
+    }
+}
